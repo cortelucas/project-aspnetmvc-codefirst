b772e2334980d24556d68fd7b8ec515b4687a6cc
Lucas Corte <corte1994.lc@gmail.com>
2021-09-17 22:31:49 -0300

    mapeamento da model livro

---
+++ src/ProjectAspNetCodeFirst/Mappings/LivroMap.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProjectAspNetCodeFirst.Models;
+
+namespace ProjectAspNetCodeFirst.Mappings
+{
+    public class LivroMap : IEntityTypeConfiguration<Livro>
+    {
+        public void Configure(EntityTypeBuilder<Livro> builder)
+        { 
+            builder.ToTable("Livro");
+
+            builder.Property(p => p.ID)
+                .ValueGeneratedNever();
+            
+            builder.Property(p => p.Titulo)
+                .HasColumnType("varchar(150)")
+                .IsRequired();
+            
+            builder.Property(p => p.Autor)
+                .HasColumnType("varchar(150)")
+                .IsRequired();
+            
+            builder.Property(p => p.Preco)
+                .HasColumnType("numeric(38,2)");
+
+            builder.Property(p => p.DataEntrada)
+                .HasColumnType("datetime");
+
+            builder.HasIndex(p => p.Titulo)
+                .HasDatabaseName("IX_Livro_Titulo");
+        }
+    }
+}
