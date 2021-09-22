e1fdd555662863304b921736cf58f493c9a46d88
Lucas Corte <corte1994.lc@gmail.com>
2021-09-22 20:05:22 -0300

    implementação da classe de categoria

--- src/ProjectAspNetCodeFirst/Models/Categoria.cs
+++ src/ProjectAspNetCodeFirst/Models/Categoria.cs
@@ -2,16 +2,20 @@ using System.Collections.Generic;
 
 namespace ProjectAspNetCodeFirst.Models
 {
-    public class Categoria
-    {
-        public Categoria(int id, string nome)
-        {
-            ID = id;
-            Nome = nome;
-        }
+  public class Categoria
+  {
+    protected Categoria() =>
+        Livros = new List<Livro>();
 
-        public int ID { get; set; }
-        public string Nome { get; set; }
-        public ICollection<Livro> Livros { get; set; }
+    public Categoria(int iD, string nome) : this()
+    {
+
+      this.ID = iD;
+      this.Nome = nome;
+
     }
+    public int ID { get; set; }
+    public string Nome { get; set; }
+    public virtual ICollection<Livro> Livros { get; set; }
+  }
 }
